9d5611eded56df93664bff25f2e0e72ba989e335
azv <dd.mazv@gmail.com>
2024-10-05 22:26:11 -0300

    commit com a resolução dos 3 primeiros desafios

---
+++ com.targetsistemas/src/desafios/valorsoma/SomaValue.java
@@ -0,0 +1,22 @@
+package desafios.valorsoma;
+
+public class SomaValue {
+	int indice;
+	int soma;
+	int k;
+	
+public SomaValue(int indice, int soma, int k) {
+	this.indice = indice;
+	this.soma = soma;
+	this.k = k;
+}
+
+	public int calc() {
+		while(k<indice) {
+			this.k = k + 1;
+			this.soma = soma + k;
+		}
+		return soma;
+	}
+	
+}
